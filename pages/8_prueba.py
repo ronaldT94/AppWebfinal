1c1821657ccabd87170099105c420bfceb9043c3
ronaldT94 <rtelloa@unsa.edu.pe>
2022-12-16 15:34:47 -0500

    Prueba

---
+++ pages/8_prueba.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.write(f'''
+    <a target="_self" href="https://docs.google.com/forms/d/e/1FAIpQLScrTq52BZIDb6iucszcxJHPw18I45Qvs8_TU_4KCvlPoUbrSg/viewform?usp=sf_link">
+        <button>
+            Please login via Google
+        </button>
+    </a>
+    ''',
+    unsafe_allow_html=True
+)

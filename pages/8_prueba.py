39f336b33f03f85f6485c2608982202e1a04816e
ronaldT94 <rtelloa@unsa.edu.pe>
2022-12-16 15:49:34 -0500

    prueba

--- pages/8_prueba.py
+++ pages/8_prueba.py
@@ -1,11 +1,7 @@
 import streamlit as st
+url = 'https://stackoverflow.com'
 
-st.write(f'''
-    <a target="_self" href="https://docs.google.com/forms/d/e/1FAIpQLScrTq52BZIDb6iucszcxJHPw18I45Qvs8_TU_4KCvlPoUbrSg/viewform?usp=sf_link">
-        <button>
-            Please login via Google
-        </button>
-    </a>
-    ''',
-    unsafe_allow_html=True
-)
+st.markdown(f'''
+<a href={url}><button style="background-color:GreenYellow;">Stackoverflow</button></a>
+''',
+unsafe_allow_html=True)
